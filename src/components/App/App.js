fdd37f6489e16fb599fb5d92a8f48079b09d0cd1
Raphael <raphael.ferreira@hetic.net>
2020-10-30 16:00:30 +0100

    add cart link

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import ArticlesPage from "../ArticlesPage/ArticlesPage";
 import ArticlePage from "../ArticlePage/ArticlePage";
+import Cart from "../Cart/Cart";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route exact path="/" component={ArticlesPage} />
         <Route exact path="/article" component={ArticlePage} />
         <Route exact path="/article/:id" component={ArticlePage} />
+        <Route exact path="/cart" component={Cart} />
         <Route component={() => "Error 404"} />
         <Redirect to="/" />
       </Switch>
