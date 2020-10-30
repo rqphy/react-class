35083e9eddf7c97b5522ef0f3ddc988bd1decea0
Raphael <raphael.ferreira@hetic.net>
2020-10-30 14:41:31 +0100

    add 404 handler and redirect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,17 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import ArticlesPage from "../ArticlesPage/ArticlesPage";
 import ArticlePage from "../ArticlePage/ArticlePage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={ArticlesPage} />
-      <Route exact path="/article" component={ArticlePage} />
-      <Route exact path="/article/:id" component={ArticlePage} />
+      <Switch>
+        <Route exact path="/" component={ArticlesPage} />
+        <Route exact path="/article" component={ArticlePage} />
+        <Route exact path="/article/:id" component={ArticlePage} />
+        <Route component={() => "Error 404"} />
+        <Redirect to="/" />
+      </Switch>
     </BrowserRouter>
   );
 }
