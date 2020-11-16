221ceac2390407725dd673d0b5d5b5094c6c6b6b
Raphael <raphael.ferreira@hetic.net>
2020-11-16 11:26:30 +0100

    complete code

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -9,27 +9,66 @@ function TicTacToe() {
     [null, null, null],
     [null, null, null],
   ]);
-  // const [ win, setWin ] = useState()
 
-  function handleClick(event) {
-    console.log(event.target);
+  function detectWin(board) {
+    if (board[0][0] === board[0][1] && board[0][0] === board[0][2]) {
+      return board[0][0];
+    }
+    return null;
   }
 
+  function handleClick(event) {
+    const { col, line } = event.target.dataset;
+    const cloneBoard = [...board];
+    // const boardLine = cloneBoard[line];
+    // const newboardLine = [...boardLine];
+    // newboardLine[col] = currentPlayer;
+    // cloneBoard[line] = newboardLine;
+    cloneBoard[line][col] = currentPlayer;
+    setBoard(cloneBoard);
+    setCurrentPlayer(currentPlayer === 1 ? 2 : 1);
+  }
+
+  function handleRestart() {
+    setCurrentPlayer(1);
+    setBoard([
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ]);
+  }
+
+  const winner = detectWin(board);
+  const win = Boolean(winner);
+
   return (
-    <div className="tic-tac-toe">
+    <div className="TicTacToe">
       {board.map((line, i) => (
-        <div>
+        <div className="TicTacToe_line">
           {line.map((col, j) => (
             <button
+              className="TicTacToe__cell"
               type="button"
-              className="tic-tac-toe__cell"
               onClick={handleClick}
+              data-col={j}
+              data-line={i}
             >
-              col
+              {col ? col : "\xa0"}
             </button>
           ))}
         </div>
       ))}
+      {!win && <div className="TicTacToe_line">Player: {currentPlayer}</div>}
+      {win && (
+        <div className="TicTacToe_line">Player {winner} win the game</div>
+      )}
+      {win && (
+        <div className="TicTacToe_line">
+          <button type="button" onClick={handleRestart}>
+            Restart
+          </button>
+        </div>
+      )}
     </div>
   );
 }
